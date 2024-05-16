be4ddfac6554b87f3036cd03f6acf37d73fe8a78
Antoine Martin <antoine@openance.com>
2024-05-16 14:25:09 +0200

    ♻️ Symplify start function

--- cmd/start.go
+++ cmd/start.go
@@ -21,7 +21,6 @@ import (
 
 	"github.com/kaweezle/iknite/pkg/alpine"
 	"github.com/kaweezle/iknite/pkg/constants"
-	"github.com/kaweezle/iknite/pkg/cri"
 	"github.com/kaweezle/iknite/pkg/k8s"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
@@ -55,8 +54,6 @@ func init() {
 	rootCmd.AddCommand(startCmd)
 
 	configureClusterCommand(startCmd.Flags(), ikniteConfig)
-	startCmd.Flags().Bool("openrc", false, "Run in OpenRC (assume OpenRC and containerd available)")
-	viper.BindPFlag("openrc", startCmd.Flags().Lookup("openrc"))
 
 	initializeKustomization(startCmd)
 }
@@ -88,27 +85,19 @@ func perform(cmd *cobra.Command, args []string) {
 
 	cobra.CheckErr(k8s.PrepareKubernetesEnvironment(ikniteConfig))
 
-	standalone := !viper.GetBool("openrc")
-
-	exist := false
-	restartProxy := false
 	// If Kubernetes is already installed, check that the configuration has not
 	// Changed.
 	config, err := k8s.LoadFromDefault()
 	if err == nil {
 		if config.IsConfigServerAddress(ikniteConfig.GetApiEndPoint()) {
 			log.Info("Kubeconfig already exists")
-			exist = true
 		} else {
 			// If the configuration has changed, we stop and disable the kubelet
 			// that may be started and clean the configuration, i.e. delete
 			// certificates and manifests.
 			log.Info("Kubernetes configuration has changed. Cleaning...")
-			cobra.CheckErr(alpine.StopService(constants.KubeletServiceName))
-			cobra.CheckErr(alpine.DisableService(constants.KubeletServiceName))
+			cobra.CheckErr(alpine.StopService(constants.IkniteService))
 			cobra.CheckErr(k8s.CleanConfig())
-			// Proxy may be left dangling
-			restartProxy = true
 		}
 	} else {
 		if !os.IsNotExist(err) {
@@ -117,66 +106,13 @@ func perform(cmd *cobra.Command, args []string) {
 		log.Info("No current configuration found. Initializing...")
 	}
 
-	if standalone {
-		// Start OpenRC
-		log.Info("Ensuring OpenRC...")
-		cobra.CheckErr(alpine.EnsureOpenRCDirectory())
-		cobra.CheckErr(alpine.StartOpenRC())
-		log.Info("Ensuring CRI...")
-		cobra.CheckErr(alpine.EnableService(constants.ContainerServiceName))
-		cobra.CheckErr(alpine.StartService(constants.ContainerServiceName))
-	}
-
-	// Enable the services
-	cobra.CheckErr(alpine.EnableService(constants.KubeletServiceName))
-	if ikniteConfig.EnableMDNS {
-		log.Info("Ensuring MDNS...")
-		cobra.CheckErr(alpine.EnableService(constants.MDNSServiceName))
-		cobra.CheckErr(alpine.StartService(constants.MDNSServiceName))
-	}
-
-	if standalone || !exist {
-		// CRI service is started by OpenRC
-		log.Info("Checking CRI is ready...")
-		available, err := cri.WaitForContainerService()
-		cobra.CheckErr(err)
-		if !available {
-			log.Fatal("CRI Service not available")
-		}
-	}
-
-	if !exist {
-		// Here we run kubeadm. This is done is two cases:
-		// - First initialization
-		// - After a configuration change. In this case, we expect kubeadm to
-		//   recreate the certificates and the manifests for the control plane.
-		log.WithFields(log.Fields{
-			"config": ikniteConfig,
-		}).Info("Running kubeadm init")
-		cobra.CheckErr(k8s.RunKubeadmInit(ikniteConfig))
-		config, err = k8s.LoadFromDefault()
-		cobra.CheckErr(err)
-		if restartProxy {
-			log.Info("Restart kube-proxy")
-			cobra.CheckErr(config.RestartProxy())
-		}
-		// We copy the configuration where the root user expects it
-		cobra.CheckErr(config.RenameConfig(ikniteConfig.ClusterName).WriteToFile(constants.KubernetesRootConfig))
-	} else if standalone {
-		// The service should have been started by OpenRC
-		log.Info("Checking kubelet...")
-		cobra.CheckErr(config.CheckClusterRunning(10, 2, 500))
-	}
-
-	if standalone {
-		// Perform the configuration. Not done in case it's already done
-		force, err := cmd.Flags().GetBool("force-config")
-		cobra.CheckErr(err)
-		cobra.CheckErr(config.DoConfiguration(ikniteConfig.Ip, force, waitTimeout))
-	} else {
-		log.Info("Enabling ", constants.ConfigureServiceName)
-		cobra.CheckErr(alpine.EnableService(constants.ConfigureServiceName))
-	}
+	// Start OpenRC
+	log.Info("Ensuring Iknite...")
+	cobra.CheckErr(alpine.EnableService(constants.IkniteService))
+	log.Info("Ensuring OpenRC...")
+	cobra.CheckErr(alpine.EnsureOpenRCDirectory())
+	cobra.CheckErr(alpine.StartOpenRC())
+	cobra.CheckErr(alpine.StartService(constants.IkniteService))
 
 	log.Info("executed")
 }
