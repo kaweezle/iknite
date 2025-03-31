4c1de6f3ea2071d31389750253318bda69531b04
Antoine Martin <antoine@mrtn.fr>
2025-03-31 14:53:31 +0200

    🚑 Fix rootfs build and bump to 5.1

--- rootfs/install_images.sh
+++ rootfs/install_images.sh
@@ -5,7 +5,7 @@ set -ex
 KUBERNETES_VERSION=$1
 
 cd /root
-apk update --quiet 
+apk update --quiet
 apk add --no-progress --no-cache fuse-overlayfs
 wget https://github.com/containerd/fuse-overlayfs-snapshotter/releases/download/v1.0.8/containerd-fuse-overlayfs-1.0.8-linux-amd64.tar.gz
 tar -xvf containerd-fuse-overlayfs-1.0.8-linux-amd64.tar.gz
@@ -28,20 +28,20 @@ export CONTAINERD_SNAPSHOTTER=fuse-overlayfs
 containerd >/var/log/containerd.log 2>&1 &
 sleep 3
 
-nerdctl --namespace=k8s.io pull -q registry.k8s.io/pause:3.8
+nerdctl --namespace=k8s.io pull -q registry.k8s.io/pause:3.10
 nerdctl --namespace=k8s.io pull -q registry.k8s.io/pause:3.9
-nerdctl --namespace=k8s.io pull -q registry.k8s.io/kube-controller-manager:v${KUBERNETES_VERSION} 
-nerdctl --namespace=k8s.io pull -q registry.k8s.io/etcd:3.5.12-0 
-nerdctl --namespace=k8s.io pull -q registry.k8s.io/kube-proxy:v${KUBERNETES_VERSION} 
-nerdctl --namespace=k8s.io pull -q registry.k8s.io/kube-scheduler:v${KUBERNETES_VERSION} 
-nerdctl --namespace=k8s.io pull -q registry.k8s.io/coredns/coredns:v1.11.1 
-nerdctl --namespace=k8s.io pull -q registry.k8s.io/kube-apiserver:v${KUBERNETES_VERSION} 
-nerdctl --namespace=k8s.io pull -q docker.io/rancher/local-path-provisioner:master-head 
-nerdctl --namespace=k8s.io pull -q quay.io/metallb/controller:v0.14.5
-nerdctl --namespace=k8s.io pull -q quay.io/metallb/speaker:v0.14.5 
-nerdctl --namespace=k8s.io pull -q registry.k8s.io/metrics-server/metrics-server:v0.7.1
-nerdctl --namespace=k8s.io pull -q docker.io/flannel/flannel:v0.25.1
-nerdctl --namespace=k8s.io pull -q docker.io/boxboat/kubectl:${KUBERNETES_VERSION}
+nerdctl --namespace=k8s.io pull -q registry.k8s.io/etcd:3.5.12-0
+nerdctl --namespace=k8s.io pull -q registry.k8s.io/kube-controller-manager:v${KUBERNETES_VERSION}
+nerdctl --namespace=k8s.io pull -q registry.k8s.io/kube-scheduler:v${KUBERNETES_VERSION}
+nerdctl --namespace=k8s.io pull -q registry.k8s.io/kube-apiserver:v${KUBERNETES_VERSION}
+nerdctl --namespace=k8s.io pull -q registry.k8s.io/kube-proxy:v${KUBERNETES_VERSION}
+nerdctl --namespace=k8s.io pull -q registry.k8s.io/coredns/coredns:v1.11.1
+nerdctl --namespace=k8s.io pull -q docker.io/rancher/local-path-provisioner:v0.0.31
+nerdctl --namespace=k8s.io pull -q registry.k8s.io/metrics-server/metrics-server:v0.7.2
+nerdctl --namespace=k8s.io pull -q ghcr.io/flannel-io/flannel:v0.26.5
+nerdctl --namespace=k8s.io pull -q ghcr.io/boxboat/kubectl:${KUBERNETES_VERSION}
+nerdctl --namespace=k8s.io pull -q ghcr.io/kube-vip/kube-vip:v0.8.9
+nerdctl --namespace=k8s.io pull -q ghcr.io/kube-vip/kube-vip-cloud-provider:v0.0.11
 
 kill %1 %2
 rm -f /var/log/containerd.log /var/log/fuse-overlayfs.log
