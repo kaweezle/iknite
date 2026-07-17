4b71835f6249de6def01dacc75c24337342dcfb0
Antoine Martin <antoine@mrtn.fr>
2026-07-17 19:45:56 +0000

    ♻️ Simplify dependency injection

--- pkg/cmd/iknitectl/di.go
+++ pkg/cmd/iknitectl/di.go
@@ -2,10 +2,9 @@ package iknitectl
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"log/slog"
-	"strings"
+	"os"
 
 	"github.com/spf13/cobra"
 	"go.uber.org/dig"
@@ -21,14 +20,8 @@ import (
 func NewContainer(opts *RootOptions) (*dig.Container, error) {
 	c := dig.New()
 
-	if opts == nil {
-		if err := provideDefaultHostAndOptions(c); err != nil {
-			return nil, fmt.Errorf("failed to provide default host and options: %w", err)
-		}
-	} else {
-		if err := provideOptionsAndHostFromOpts(c, opts); err != nil {
-			return nil, fmt.Errorf("failed to provide options and host from opts: %w", err)
-		}
+	if err := provideOptionsAndHostFromOpts(c, opts); err != nil {
+		return nil, fmt.Errorf("failed to provide options and host from opts: %w", err)
 	}
 
 	if err := c.Provide(utils.NewHookManager); err != nil {
@@ -59,6 +52,14 @@ func NewContainer(opts *RootOptions) (*dig.Container, error) {
 		return nil, fmt.Errorf("failed to provide logger: %w", err)
 	}
 
+	if err := c.Provide(context.Background); err != nil {
+		return nil, fmt.Errorf("failed to provide context: %w", err)
+	}
+
+	if err := c.Provide(func() types.CmdOut { return os.Stdout }); err != nil {
+		return nil, fmt.Errorf("failed to provide CmdOut: %w", err)
+	}
+
 	return c, nil
 }
 
@@ -82,58 +83,33 @@ func newStore(c *config.Config, hm *utils.HookManager) (*db.Store, error) {
 	return store, nil
 }
 
-func provideDefaultHostAndOptions(c *dig.Container) error {
-	if err := c.Provide(
-		host.NewDefaultHost,
-		dig.As(
-			new(host.FileEnvironment),
-			new(host.Environment),
-			new(host.FileExecutor),
-			new(host.NetworkHost),
-			new(host.System),
-			new(host.FileSystem),
-		),
-	); err != nil {
-		return fmt.Errorf("failed to provide host: %w", err)
-	}
-
-	if err := c.Provide(func(fe host.FileEnvironment) (host.Host, error) {
-		feAsHost, ok := fe.(host.Host)
-		if !ok {
-			return nil, fmt.Errorf("provided FileEnvironment does not implement Host")
-		}
-		return feAsHost, nil
-	}); err != nil {
-		return fmt.Errorf("failed to provide host as FileEnvironment: %w", err)
-	}
-
-	if err := c.Provide(NewRootOptions); err != nil {
-		return fmt.Errorf("failed to provide RootOptions: %w", err)
-	}
-
-	return nil
-}
+var hostInterfacesOption = dig.As(
+	new(host.FileEnvironment),
+	new(host.Environment),
+	new(host.FileExecutor),
+	new(host.NetworkHost),
+	new(host.System),
+	new(host.FileSystem),
+)
 
 func provideOptionsAndHostFromOpts(c *dig.Container, opts *RootOptions) error {
-	if err := c.Provide(func() host.Host { return opts.host }); err != nil {
-		return fmt.Errorf("failed to provide host: %w", err)
-	}
-	if err := c.Provide(func() host.Host { return opts.host },
-		dig.As(
-			new(host.Host),
-			new(host.FileEnvironment),
-			new(host.Environment),
-			new(host.FileExecutor),
-			new(host.NetworkHost),
-			new(host.System),
-			new(host.FileSystem),
-		)); err != nil {
-		return fmt.Errorf("failed to provide host: %w", err)
+	if opts == nil {
+		if err := c.Provide(func() *RootOptions { return NewRootOptions(nil) }); err != nil {
+			return fmt.Errorf("failed to provide RootOptions: %w", err)
+		}
+	} else {
+		if err := c.Provide(func() *RootOptions { return opts }); err != nil {
+			return fmt.Errorf("failed to provide RootOptions: %w", err)
+		}
 	}
 
-	if err := c.Provide(func() *RootOptions { return opts }); err != nil {
-		return fmt.Errorf("failed to provide RootOptions: %w", err)
+	if err := c.Provide(func(opts *RootOptions) host.Host { return opts.host }); err != nil {
+		return fmt.Errorf("failed to provide host: %w", err)
 	}
+	if err := c.Provide(func(opts *RootOptions) host.Host { return opts.host }, hostInterfacesOption); err != nil {
+		return fmt.Errorf("failed to provide host with interfaces: %w", err)
+	}
+
 	return nil
 }
 
@@ -166,30 +142,22 @@ func ProvideCommand(c Provider, cmd *cobra.Command, args []string) error {
 	if err := c.Provide(func() types.CmdArgs { return args }); err != nil {
 		return fmt.Errorf("failed to provide args: %w", err)
 	}
-	if err := c.Provide(func(command *cobra.Command) types.CmdOut { return command.OutOrStdout() }); err != nil {
-		if strings.Contains(errors.Unwrap(err).Error(), "already provided") {
-			err = c.Decorate(func(_ types.CmdOut, command *cobra.Command) types.CmdOut { return command.OutOrStdout() })
-		}
-		if err != nil {
-			return fmt.Errorf("failed to provide CmdOut: %w", err)
-		}
+	if err := c.Decorate(
+		func(_ types.CmdOut, command *cobra.Command) types.CmdOut {
+			return command.OutOrStdout()
+		},
+	); err != nil {
+		return fmt.Errorf("failed to provide CmdOut: %w", err)
 	}
 	if err := c.Provide(func() types.CmdIn { return cmd.InOrStdin() }); err != nil {
 		return fmt.Errorf("failed to provide CmdIn: %w", err)
 	}
-	if err := c.Provide(
-		func(command *cobra.Command) context.Context { return command.Context() },
+	if err := c.Decorate(
+		func(_ context.Context, command *cobra.Command) context.Context {
+			return command.Context()
+		},
 	); err != nil {
-		if strings.Contains(errors.Unwrap(err).Error(), "already provided") {
-			err = c.Decorate(
-				func(_ context.Context, command *cobra.Command) context.Context {
-					return command.Context()
-				},
-			)
-		}
-		if err != nil {
-			return fmt.Errorf("failed to provide context: %w", err)
-		}
+		return fmt.Errorf("failed to provide context: %w", err)
 	}
 
 	return nil
